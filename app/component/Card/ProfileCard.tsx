affb5ae7c704ad5e94ce48dff34862c1888863ac
theNubiadev <ajaiyeobajibola@gmail.com>
2025-12-19 12:28:28 +0100

    feat: fixed the bug affetcting the display

--- app/component/Card/ProfileCard.tsx
+++ app/component/Card/ProfileCard.tsx
@@ -10,8 +10,8 @@ export default function ProfileCard({ image, name, position }: ProfileCardProps)
       className="rounded-[20px] relative lg:h-100 h-67.5 justify-center bg-cover bg-center bg-no-repeat overflow-hidden"
       style={{ backgroundImage: `url(${image})` }}
     >
-      <div 
-        className="top-0 left-0 absolute h-full w-full p-2 flex items-end" 
+      <div
+        className="top-0 left-0 absolute h-full w-full p-2 flex items-end"
         style={{ background: `rgba(0,0,0,0.3)` }}
       >
         <div className="border flex items-center flex-col border-white w-full py-2.5 px-2.75 lg:py-3.75 lg:px-4 glassmorphism_white rounded-[7px] lg:rounded-bl-[13px] lg:rounded-tr-[13px] bottom-0">
