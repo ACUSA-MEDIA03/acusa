e857a197acd81e99f9a2beb145f4ebea0a82855c
theNubiadev <ajaiyeobajibola@gmail.com>
2025-12-21 23:10:25 +0100

    i added the react-paginate depenency and the podcast

---
+++ app/component/Card/PodcastCard.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+interface PodcastCardProps {
+  audioSource: string;
+}
+
+const PodcastCard = ({ audioSource }: PodcastCardProps) => {
+  return (
+    <div className="lg:w-[70%] w-full p-4 border border-red-500 rounded-[20px] flex space-x-5">
+      {/* Icon / Avatar */}
+      <div className="rounded-full border w-[70px] h-[70px] bg-red-500 grid place-content-center items-center">
+        {/* optional podcast icon */}
+      </div>
+
+      {/* Audio Player */}
+      <div className="basis-[90%] flex items-center">
+        <audio
+          src={audioSource}
+          controls
+          className="w-full border"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PodcastCard;
