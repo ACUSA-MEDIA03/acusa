2a2d1bade71393605c6826bd8b651d97ed4e4c69
theNubiadev <ajaiyeobajibola@gmail.com>
2025-12-28 23:05:01 +0100

    Add Admin component for the admin dashboard

---
+++ app/admin/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+export default function Admin(){
+    return (
+        <>
+            <div className="mt-12">
+      <h1 className="text-2xl font-bold text-main">Admin Dashboard</h1>
+      <p>Welcome to the admin panel</p>
+    </div>
+        </>
+    )
+}
