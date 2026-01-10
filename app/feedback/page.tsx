2627804ead4d0d56e4dfed789180a8f49825a4d1
theNubiadev <ajaiyeobajibola@gmail.com>
2026-01-10 17:30:53 +0100

    fix: correct import path for Navbar component in Feedback page

--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -1,14 +1,39 @@
 "use client"
-import Navbar from "@/component/Navbar"
-import Footer from "@/component/Footer"
+import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
 
 
 export default function Feedback() {
     return (
         <>
            <Navbar />
-            
+             <div className="max-w-md mx-auto mt-20 space-y-4">
+     <h2 className="text-2xl font-bold">Login</h2>
 
+      <input
+        type="email"
+        placeholder="Email"
+        // value={email}
+        // onChange={(e) => setEmail(e.target.value)}
+        className="w-full border p-2 rounded"
+        // disabled={loading}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        // value={password}
+        // onChange={(e) => setPassword(e.target.value)}
+        className="w-full border p-2 rounded"
+        // disabled={loading}
+        // onKeyPress={(e) => {
+        //   if (e.key === "Enter") {
+            // handleLogin(e);
+        //   }
+        // }}
+      />
+
+</div>
             <Footer />
             </>
     )
