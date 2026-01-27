7f0fcb547aa8aa9d6373debcba828909d4fd4216
theNubiadev <ajaiyeobajibola@gmail.com>
2026-01-27 17:32:23 +0100

    now worked on the image display from cloudinary

--- components/PublicationSection/OfficialLetter.tsx
+++ components/PublicationSection/OfficialLetter.tsx
@@ -47,58 +47,9 @@ export default function OfficialLetter({recipient, ref}: OfficialLetterProp){
         <div className="font-grotesk leading-7.5 space-y-4">
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, ea
-            harum quo iusto omnis temporibus aspernatur deserunt fugit tempora
-            aliquam cum, asperiores in dolorem maxime ratione sapiente fuga
-            officia magnam, impedit magni unde! Quis necessitatibus architecto
-            quidem quaerat magnam accusantium, maxime at doloribus aperiam,
-            officiis debitis suscipit non placeat eius, natus ullam quas optio
-            numquam quam minima odio autem nobis earum in! Debitis minus natus,
-            placeat impedit deleniti deserunt harum illum veniam dolorem
-            dignissimos facere cupiditate? Ullam sed autem magni veniam
-            voluptate sequi facere quaerat eum vero maxime ipsa, culpa in beatae
-            neque ad doloremque doloribus aut, non vitae? Laboriosam sit sunt
-            ullam excepturi maiores! Vel modi nisi, in consectetur est
-            asperiores dolore debitis laboriosam cum. Accusantium culpa pariatur
-            sit explicabo veniam, nesciunt aperiam ad, corporis aliquid impedit
-            minima odit temporibus libero blanditiis ut non dicta vero?
-            Asperiores, incidunt dolorem.
+            harum quo iusto omnis temporibus aspernatur deserunt 
           </p>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, ea
-            harum quo iusto omnis temporibus aspernatur deserunt fugit tempora
-            aliquam cum, asperiores in dolorem maxime ratione sapiente fuga
-            officia magnam, impedit magni unde! Quis necessitatibus architecto
-            quidem quaerat magnam accusantium, maxime at doloribus aperiam,
-            officiis debitis suscipit non placeat eius, natus ullam quas optio
-            numquam quam minima odio autem nobis earum in! Debitis minus natus,
-            placeat impedit deleniti deserunt harum illum veniam dolorem
-            dignissimos facere cupiditate? Ullam sed autem magni veniam
-            voluptate sequi facere quaerat eum vero maxime ipsa, culpa in beatae
-            neque ad doloremque doloribus aut, non vitae? Laboriosam sit sunt
-            ullam excepturi maiores! Vel modi nisi, in consectetur est
-            asperiores dolore debitis laboriosam cum. Accusantium culpa pariatur
-            sit explicabo veniam, nesciunt aperiam ad, corporis aliquid impedit
-            minima odit temporibus libero blanditiis ut non dicta vero?
-            Asperiores, incidunt dolorem.
-          </p>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, ea
-            harum quo iusto omnis temporibus aspernatur deserunt fugit tempora
-            aliquam cum, asperiores in dolorem maxime ratione sapiente fuga
-            officia magnam, impedit magni unde! Quis necessitatibus architecto
-            quidem quaerat magnam accusantium, maxime at doloribus aperiam,
-            officiis debitis suscipit non placeat eius, natus ullam quas optio
-            numquam quam minima odio autem nobis earum in! Debitis minus natus,
-            placeat impedit deleniti deserunt harum illum veniam dolorem
-            dignissimos facere cupiditate? Ullam sed autem magni veniam
-            voluptate sequi facere quaerat eum vero maxime ipsa, culpa in beatae
-            neque ad doloremque doloribus aut, non vitae? Laboriosam sit sunt
-            ullam excepturi maiores! Vel modi nisi, in consectetur est
-            asperiores dolore debitis laboriosam cum. Accusantium culpa pariatur
-            sit explicabo veniam, nesciunt aperiam ad, corporis aliquid impedit
-            minima odit temporibus libero blanditiis ut non dicta vero?
-            Asperiores, incidunt dolorem.
-          </p>
+     
         </div>
       </div>
       {/*  */}
