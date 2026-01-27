e70a26e714ff7603f307d05505e1f1694c2ec6a4
theNubiadev <ajaiyeobajibola@gmail.com>
2026-01-27 17:09:51 +0100

    feat: wrote the logic for image upload for fast storage and delivery

---
+++ app/api/upload/route.ts
@@ -0,0 +1,24 @@
+import cloudinary from "@/lib/cloudinary";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const data = await req.json(); // receive base64 or URL
+    const { file } = data;
+
+    if (!file) {
+      return NextResponse.json({ error: "No file provided" }, { status: 400 });
+    }
+
+    // Upload to Cloudinary
+    const result = await cloudinary.uploader.upload(file, {
+      folder: "official_letters", // optional folder
+      resource_type: "auto", // allows image, video, or audio
+    });
+
+    return NextResponse.json({ url: result.secure_url });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ error: "Upload failed" }, { status: 500 });
+  }
+};
