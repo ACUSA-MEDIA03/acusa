60707f8a305882753c4677e51cf2bbc48f8752a8
theNubiadev <ajaiyeobajibola@gmail.com>
2026-01-28 12:51:06 +0100

    fix: update import path for FileUpload component in Newsletter and OfficialLettersTab components

--- components/PublicationSection/officialletters-tab.tsx
+++ components/PublicationSection/officialletters-tab.tsx
@@ -17,7 +17,7 @@ import {
   ChevronRight,
 } from "lucide-react";
 import Image from "next/image";
-import { FileUpload } from "@/components/card/fileupload";
+import { FileUpload } from "@/components/card/temp";
 import { toast } from "sonner";
 
 interface OfficialLetter {
